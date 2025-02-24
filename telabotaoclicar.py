4d04be25e0d4c8f49f811ef620443bbd02e873d8
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-24 19:38:26 -0300

    Update telabotaoclicar.py

--- telabotaoclicar.py
+++ telabotaoclicar.py
@@ -8,17 +8,22 @@ def bt_click():
 def bt_clickar():
     #esse print exibe o testo digitaod na caixa de texto e exibe na label da tela
     print(ed.get())
-    lb=["text"]=ed.get()
+    lb['text']=ed.get()
 #i (instanciar) recebe o objeto Tk
 i=Tk()
 #gerar titulo na janela
 i.title('Programa financeiro')
 i.geometry('980X720+250+30')
-i[bg]='dark red'
+i["bg"]='dark red'
 #i.wm._iconbitmap('icone.ico')
 lb=Label(i,text='Nome do Usuario')
 lb.place(x=100,y=100)
 bt=Button(i,width="20",text='OK',command=bt_click)
 bt.place(x=230,y=100)
 #o condigo abaixo cria um botão que posiciona abaixo do botão OK
-bt=Button(i,width="20")
+bt=Button(i,width="20",text='Capturar',command=bt_click)
+bt.place(x=230,y=190)
+#cria a caixa de texo para digitar algo dentro
+ed=Entry(i)
+ed.place(x=230,y=150)
+i.mainloop()
