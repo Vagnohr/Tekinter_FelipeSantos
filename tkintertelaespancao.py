862a72e49badd4bd505870b834bcaeec03f45fd8
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-24 21:42:51 -0300

    Update tkintertelaespancao.py

--- tkintertelaespancao.py
+++ tkintertelaespancao.py
@@ -11,6 +11,7 @@ lb3.place(x=230,y=200)
 lb4=Label(i,text="Label 4",bg="red")
 lb4.place(x=230,y=200)
 lb1.pack(side=TOP,fill=X)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
-lb2.pack(side=RIGHT,)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
-lb3.pack(side=TOP,fill=X)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
-lb4.pack(side=TOP,fill=X)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
+lb2.pack(side=RIGHT,fill=Y)#comando fill é responsavel do preenchimento 100%, deve usar y para vertical e deve ser maiusculo
+lb3.pack(side=TOP,fill=Y)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
+lb4.pack(side=RIGHT,fill=X)#comando fill é responsavel do preenchimento 100%, deve usar x para horizontal e deve ser maiusculo
+i.mainloop()
