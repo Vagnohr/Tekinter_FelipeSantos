0c2a0248a26f17ceb144b7fb7ae7cb067ccaed02
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-24 21:21:18 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -14,8 +14,12 @@ lb4.place(x=230,y=200)
 lb2.pack()
 lb3.pack()
 lb4.pack()"""
+#o codigo abaixo é responsavel por posicionar a label no topo da interface
 lb1.pack(side="top")
+#o codigo abaixo é responsavel por posicionar a label na esquerda da interface
 lb2.pack(side="left")
+#o codigo abaixo é responsavel por posicionar a label na direita da interface
 lb3.pack(side="right")
+#o codigo abaixo é responsavel por posicionar a label no inferior da interface
 lb4.pack(side="bottom")
 i.mainloop()
